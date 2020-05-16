6b8ad1d6777bc5d22abfcf0de71660a45179590e
surendra <vadaparthysurendra@gmail.com>
2020-05-16 18:21:16 +0530

    Updates

---
+++ precache-manifest.ad4688ad81969d313c35bfcc19688909.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883f3e8ce30dbd21269d85f8389157f9",
+    "url": "/Myportfolio/index.html"
+  },
+  {
+    "revision": "1f4aebd016ebf8db0278",
+    "url": "/Myportfolio/static/css/main.3370ef35.chunk.css"
+  },
+  {
+    "revision": "345d24ac928cf087b424",
+    "url": "/Myportfolio/static/js/2.a2366cee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Myportfolio/static/js/2.a2366cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4aebd016ebf8db0278",
+    "url": "/Myportfolio/static/js/main.05e0d64b.chunk.js"
+  },
+  {
+    "revision": "d86dd74e5d4930855c66",
+    "url": "/Myportfolio/static/js/runtime-main.d7842963.js"
+  },
+  {
+    "revision": "d83c531a58366697fafe62db439627af",
+    "url": "/Myportfolio/static/media/Administrator.d83c531a.png"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/Myportfolio/static/media/Roboto-BlackItalic.99682a78.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/Myportfolio/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  },
+  {
+    "revision": "52bec8f6a5fb989a25429cd17b52c036",
+    "url": "/Myportfolio/static/media/about-us.52bec8f6.png"
+  },
+  {
+    "revision": "50953027678f2e493ea34b63733f26cd",
+    "url": "/Myportfolio/static/media/banner-image.50953027.png"
+  }
+]);
