b9b1492ddeb73158df473c7a652df03534efc876
Vadaparthy Surendra <37802344+surendravj@users.noreply.github.com>
2021-01-06 12:29:03 +0530

    Updated tech stack UI

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,32 +1,48 @@
 import React from "react";
 import "../index.css";
+import { Card } from "ui-neumorphism";
+
 const Skill = () => {
   const skills = [
-    "C/C++",
+    "C",
+    "C++",
     "Python",
     "Javascript",
     "Dart",
     "HTML5",
     "CSS3",
-    "Boostrap",
+    "Bootstrap",
+    "NPM",
     "ReactJs",
+    "Redux",
     "NodeJs",
+    "express",
     "Flutter",
-    "SQL",
+    "MySql",
     "Mongodb",
     "Firebase",
-    "openCV",
-    "Machine learning",
+    "AWS",
+    "Github",
+    "TensorFlow"
   ];
 
   const skillDiv = () => {
     return skills.map((skill, index) => {
       return (
         <div
-          className="col-lg-3 col-md-3 col-sm-2 col-xs-3 skill ml-1 mb-1 text-center"
+          className="col-lg-2 col-md-2 col-sm-2 col-xs-2 ml-1 mb-3 text-center"
           key={index}
         >
-          <div className="text-center mt-2 mb-2">{skill}</div>
+          <Card className="skill-card text-center bg-white p-2" elevation={3}>
+            <img
+              width="40"
+              height="40"
+              src={require(`../assets/images/logos/${skill}.svg`)}
+              alt="dfs"
+              className="img-fluid"
+            />
+            <div className="skill-text">{skill}</div>
+          </Card>
         </div>
       );
     });
@@ -35,7 +51,7 @@ const Skill = () => {
   return (
     <div className="container text-center" data-aos="zoom-in">
       <h3 className="service-head">My tech stack</h3>
-      <div className="row offset-md-2">
+      <div className="row offset-md-2 mt-3">
         <div className="col-md-12">
           <div className="row">{skillDiv()}</div>
         </div>
