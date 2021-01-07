1ae782603d5596a07ca4fc1bb7f5b9003b568cc9
Vadaparthy Surendra <37802344+surendravj@users.noreply.github.com>
2021-01-07 13:01:14 +0530

    Updated github page ui

--- src/components/Github_Table.js
+++ src/components/Github_Table.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../resume.css";
+import { Card } from "ui-neumorphism";
+
 const Table = ({ data }) => {
   function showMsg(repoUrl) {
     alert("Clone link copied to clipboard");
@@ -9,35 +11,42 @@ const Table = ({ data }) => {
   const renderColumn = () => {
     return data.map((repo, i) => {
       return (
-        <div
-          className=" col-lg-3 card rounded shadow-lg col-md-3 col-sm-3 col-xs-12 github-div mr-1 mb-1"
-          key={i}
-        >
-          <div className="github-name text-center">
-            <p>{repo.full_name}</p>
+        
+          <div
+            className=" col-lg-3 col-md-3 col-sm-3 col-xs-12 mr-1 mb-1"
+            key={i}
+          >
+            <Card className="p-3 github-div" elevation={1}>
+              <div className="github-name text-center">
+                <p>{repo.full_name}</p>
+              </div>
+              <div className=" text-center mb-2">
+                <span className="badge github-badge ">{repo.language!=null?repo.language:'Javascript'}</span>
+              </div>
+              <div className="github-buttons">
+                <a
+                  href={repo.svn_url}
+                  target="_blank"
+                  rel="noopener noreferrer "
+                >
+                  Github
+                </a>
+                <button
+                  className="click-copy"
+                  onClick={() => showMsg(repo.clone_url)}
+                >
+                  Clone
+                </button>
+                <a
+                  href="https://github.com/surendravj"
+                  target="_blank"
+                  rel="noopener noreferrer "
+                >
+                  Profile
+                </a>
+              </div>
+            </Card>
           </div>
-          <div className=" text-center mb-2">
-            <span className="badge github-badge ">{repo.language}</span>
-          </div>
-          <div className="github-buttons">
-            <a href={repo.svn_url} target="_blank" rel="noopener noreferrer ">
-              Github
-            </a>
-            <button
-              className="click-copy"
-              onClick={() => showMsg(repo.clone_url)}
-            >
-              Clone
-            </button>
-            <a
-              href="https://github.com/surendravj"
-              target="_blank"
-              rel="noopener noreferrer "
-            >
-              Profile
-            </a>
-          </div>
-        </div>
       );
     });
   };
